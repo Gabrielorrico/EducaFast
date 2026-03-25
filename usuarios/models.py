ed2564a5dafea9e22aaa6c0f8800bf4dee2372e2
brncluis <fockluis@gmail.com>
2026-03-25 18:53:18 -0300

    inicio app usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+class PerfilUsuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    meta_horas_estudo = models.PositiveIntegerField(default=2)  
+    data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.nome
+        return f'Perfil de {self.usuario.username}'
