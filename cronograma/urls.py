4720620d54a56147b2ccf3052d233a91fff6c8b1
Gabrielorrico <agbo@cesar.school>
2026-03-28 15:51:58 -0300

    Atualizando o views e as urls.

--- cronograma/urls.py
+++ cronograma/urls.py
@@ -5,4 +5,8 @@ app_name = 'cronograma'
 
 urlpatterns = [
     path('', views.listar_cronogramas, name='listar_cronogramas'),
+    path('<int:cronograma_id>/', views.detalhe_cronograma, name='detalhe'),
+    path('<int:cronograma_id>/sessao/criar/', views.criar_sessao, name='criar_sessao'),
+    path('sessao/<int:sessao_id>/concluir/', views.concluir_sessao, name='concluir_sessao'),
+    path('sessao/<int:sessao_id>/deletar/', views.deletar_sessao, name='deletar_sessao'),
 ]
