6d658af0dff7d6b0e3026734a6fb3c5e9e706036
brncluis <fockluis@gmail.com>
2026-04-08 11:49:51 -0300

    aba nova

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ python manage.py loaddata materias_iniciais.json
 python manage.py importar_provas
 python manage.py collectstatic --noinput
 python manage.py popular_flashcards
+python manage.py loaddata inicial-data.json
